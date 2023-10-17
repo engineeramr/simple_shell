0ec90c42c3797fe974e23ea2ef77ff84394133ba
Amr Aly <amr83160@gmail.com>
2023-10-18 02:57:47 +0300

    edited

---
+++ _strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ **_strchr - locates a character in string
+ *
+ *@s: string
+ *@c: character
+ *
+ *Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+		{
+			return (s);
+		}
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
